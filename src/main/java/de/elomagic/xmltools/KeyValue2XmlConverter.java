43ebc1501534203671a1218e77f67db1faeb142d
C. Rambow <rambow.dev@elomagic.de>
2023-07-28 20:55:23 +0200

    Element support added

--- src/main/java/de/elomagic/xmltools/KeyValue2XmlConverter.java
+++ src/main/java/de/elomagic/xmltools/KeyValue2XmlConverter.java
@@ -1,7 +1,6 @@
 package de.elomagic.xmltools;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -10,18 +9,16 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class KeyValue2XmlConverter {
 
     private String keyDelimiter = ".";
-
-    private boolean attributeSupport = true;
-    private Pattern attributeDelimiterRegEx = Pattern.compile(".+#(.*)$");
-
+    private Pattern keyPattern = Pattern.compile("^(?<name>[^#\\[\\]]+)(\\[(?<index>\\d+)])?(#(?<attr>.+))?$");
     private int repetitionStart = 1;
-    private Pattern repetitionRegExPattern = Pattern.compile(".+\\[(\\d)\\]$");
 
     public String getKeyDelimiter() {
         return keyDelimiter;
@@ -31,20 +28,12 @@ public class KeyValue2XmlConverter {
         this.keyDelimiter = keyDelimiter;
     }
 
-    public Pattern getAttributeDelimiterRegEx() {
-        return attributeDelimiterRegEx;
+    public Pattern getKeyPattern() {
+        return keyPattern;
     }
 
-    public void setAttributeDelimiterRegEx(Pattern attributeDelimiterRegEx) {
-        this.attributeDelimiterRegEx = attributeDelimiterRegEx;
-    }
-
-    public boolean isAttributeSupport() {
-        return attributeSupport;
-    }
-
-    public void setAttributeSupport(boolean attributeSupport) {
-        this.attributeSupport = attributeSupport;
+    public void setKeyPattern(Pattern keyPattern) {
+        this.keyPattern = keyPattern;
     }
 
     public int getRepetitionStart() {
@@ -53,14 +42,6 @@ public class KeyValue2XmlConverter {
 
     public void setRepetitionStart(int repetitionStart) {
         this.repetitionStart = repetitionStart;
-    }
-
-    public Pattern getRepetitionRegExPattern() {
-        return repetitionRegExPattern;
-    }
-
-    public void setRepetitionRegExPattern(Pattern repetitionRegExPattern) {
-        this.repetitionRegExPattern = repetitionRegExPattern;
     }
 
     @NotNull
@@ -89,20 +70,40 @@ public class KeyValue2XmlConverter {
         //document.getDocumentElement().
 
         Node element = document;
-        //document.appendChild(document.createElement("bbbb"));
 
-        for (String name : keyChain) {
-            element = findAnyChild(element, name)
-                    .orElse((Element)element.appendChild(document.createElement(name)));
+        for (String item : keyChain) {
+            Matcher matcher = keyPattern.matcher(item);
+
+            if (matcher.find()) {
+                String name = matcher.group("name");
+                int index = Integer.valueOf(Objects.toString(matcher.group("index"), "0"));
+                // TODO Check. Attr can only be set on latest item
+                String attr = matcher.group("attr");
+
+                Optional<Element> oe = findAnyChild(element, name);
+                if (oe.isEmpty()) {
+                    element = element.appendChild(document.createElement(name));
+                } else {
+                    element = oe.get();
+                }
+
+                if (attr != null) {
+                    // TODO element = ((Element)element).setAttributeNode(document.createAttribute(attr));
+                }
+            } else {
+                throw new RuntimeException("Unsupported key value '" + key + "'.");
+            }
         }
 
         element.appendChild(document.createTextNode(value));
-
     }
 
     @NotNull
     Optional<Element> findAnyChild(@NotNull Node parent, @NotNull String name) {
-        return ElementTool.streamChildElements(parent).filter(c -> c.getNodeName().equals(name)).findAny();
+        return ElementTool
+                .streamChildElements(parent)
+                .filter(c -> c.getNodeName().equals(name))
+                .findAny();
     }
 
 }
