74ff3c4ca4a53a029a7f638e597d9fe2514f76b2
Carsten Rambow <dev@elomagic.de>
2023-07-28 15:46:12 +0200

    First draft

--- src/main/java/de/elomagic/xmltools/KeyValue2XmlConverter.java
+++ src/main/java/de/elomagic/xmltools/KeyValue2XmlConverter.java
@@ -3,28 +3,25 @@ package de.elomagic.xmltools;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.lang.annotation.Documented;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 public class KeyValue2XmlConverter {
 
     private String keyDelimiter = ".";
 
     private boolean attributeSupport = true;
-    private String attributeDelimiter = "#";
+    private Pattern attributeDelimiterRegEx = Pattern.compile(".+#(.*)$");
 
     private int repetitionStart = 1;
-    private String repetitionPattern = "[%s]";
+    private Pattern repetitionRegExPattern = Pattern.compile(".+\\[(\\d)\\]$");
 
     public String getKeyDelimiter() {
         return keyDelimiter;
@@ -34,8 +31,16 @@ public class KeyValue2XmlConverter {
         this.keyDelimiter = keyDelimiter;
     }
 
-    public String getAttributeDelimiter() {
-        return attributeDelimiter;
+    public Pattern getAttributeDelimiterRegEx() {
+        return attributeDelimiterRegEx;
+    }
+
+    public void setAttributeDelimiterRegEx(Pattern attributeDelimiterRegEx) {
+        this.attributeDelimiterRegEx = attributeDelimiterRegEx;
+    }
+
+    public boolean isAttributeSupport() {
+        return attributeSupport;
     }
 
     public void setAttributeSupport(boolean attributeSupport) {
@@ -50,6 +55,14 @@ public class KeyValue2XmlConverter {
         this.repetitionStart = repetitionStart;
     }
 
+    public Pattern getRepetitionRegExPattern() {
+        return repetitionRegExPattern;
+    }
+
+    public void setRepetitionRegExPattern(Pattern repetitionRegExPattern) {
+        this.repetitionRegExPattern = repetitionRegExPattern;
+    }
+
     @NotNull
     public Optional<Document> convert(@NotNull Map<String, String> keyValueMap) throws ParserConfigurationException {
 
@@ -62,6 +75,7 @@ public class KeyValue2XmlConverter {
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.newDocument();
 
+        // TODO Sort keys
         keyValueMap.forEach((k, v) -> mapKeyValue(k, v, doc));
 
         return Optional.of(doc);
@@ -70,15 +84,25 @@ public class KeyValue2XmlConverter {
 
     public void mapKeyValue(@NotNull String key, @NotNull String value, @NotNull Document document) {
 
-        String[] items = key.split(keyDelimiter);
+        String[] keyChain = key.split("\\.");
 
         //document.getDocumentElement().
 
-        for (String item : items) {
-            //document.getChildNodes()
+        Node element = document;
+        //document.appendChild(document.createElement("bbbb"));
+
+        for (String name : keyChain) {
+            element = findAnyChild(element, name)
+                    .orElse((Element)element.appendChild(document.createElement(name)));
         }
 
+        element.appendChild(document.createTextNode(value));
 
+    }
+
+    @NotNull
+    Optional<Element> findAnyChild(@NotNull Node parent, @NotNull String name) {
+        return ElementTool.streamChildElements(parent).filter(c -> c.getNodeName().equals(name)).findAny();
     }
 
 }
---
+++ src/test/java/de/elomagic/xmltools/KeyValue2XmlConverterTest.java
@@ -0,0 +1,58 @@
+package de.elomagic.xmltools;
+
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KeyValue2XmlConverterTest {
+
+    @Test
+    void testConvert() throws ParserConfigurationException, IOException, SAXException {
+
+        Properties properties = new Properties();
+        properties.load(getClass().getResourceAsStream("/sample01.properties"));
+
+        Map<String, String> map = new HashMap<>();
+        properties.forEach((key, value) -> map.put(key.toString(), Objects.toString(value, "")));
+
+        KeyValue2XmlConverter converter = new KeyValue2XmlConverter();
+        Document document = converter.convert(map).orElseThrow();
+
+        toString(document);
+
+        assertEquals("ddd", document.getDocumentElement().getNodeName());
+    }
+
+    public static String toString(Document doc) {
+        try {
+            StringWriter sw = new StringWriter();
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+            transformer.transform(new DOMSource(doc), new StreamResult(sw));
+            System.out.println("XML=" + sw);
+            return sw.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException("Error converting to String", ex);
+        }
+    }
+}
